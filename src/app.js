d4ab80e9ce6eca51666868f3c9203e42730505b0
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2023-05-21 12:24:15 +0800

    Add routes to get task by ID

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const router = require("./controller/router");
+const { ValidationError } = require("express-validation");
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
@@ -11,8 +13,15 @@ app.use("/ping", (req, res, next) => {
   res.status(200).send({ pong: true });
 });
 
+app.use(router);
+
 // Error handling middleware
 app.use((err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    res.status(err.statusCode);
+    res.send({ error: err.details });
+    return;
+  }
   console.error(err);
   res.status(500).json({ error: "Internal Server Error" });
 });
---
+++ src/controller/router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const taskRouter = require('./task');
+
+router.use('/task', taskRouter);
+
+module.exports = router;
