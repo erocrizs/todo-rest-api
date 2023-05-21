26c94d6a75974e025717465965af23efb39c43b4
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2023-05-21 12:39:20 +0800

    Remove unused test line

--- src/controller/task/index.js
+++ src/controller/task/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const validateTaskId = require("@src/middleware/validate-task-id");
 const getTaskById = require("./get-by-id");
 
-router.get("/taste", (req, res) => res.status(200).send({ taste: true }));
 router.all("/:id", validateTaskId);
 router.get("/:id", getTaskById.handler);
 
