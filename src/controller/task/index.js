d074661577868a46fa8cddf8161e3255b289092c
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2023-05-21 19:28:46 +0800

    Implement endpoint for deleting a task

--- src/controller/task/index.js
+++ src/controller/task/index.js
@@ -5,11 +5,13 @@ const getTaskById = require("./get-by-id");
 const listTasks = require("./list");
 const createTask = require("./create");
 const updateTask = require("./update");
+const deleteTask = require("./delete");
 
 router.get("/", listTasks.validator, listTasks.handler);
 router.post("/", createTask.validator, createTask.handler);
 router.all("/:id", validateTaskId);
 router.get("/:id", getTaskById.handler);
 router.put("/:id", updateTask.validator, updateTask.handler);
+router.delete("/:id", deleteTask.handler);
 
 module.exports = router;
