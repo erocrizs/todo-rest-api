02da431ceec234e5bdb0b2ffe12f219fda22be7a
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2023-05-21 17:25:22 +0800

    Create endpoint for creating new tasks

--- src/controller/task/index.js
+++ src/controller/task/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const validateTaskId = require("@src/middleware/validate-task-id");
 const getTaskById = require("./get-by-id");
 const listTasks = require("./list");
+const createTask = require("./create");
 
 router.get("/", listTasks.validator, listTasks.handler);
+router.post("/", createTask.validator, createTask.handler);
 router.all("/:id", validateTaskId);
 router.get("/:id", getTaskById.handler);
 
