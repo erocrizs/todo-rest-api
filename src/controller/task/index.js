efc4f92f037e946600cc129cf6c941a92759e40f
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2023-05-21 18:51:55 +0800

    Implement endpoint to update a task

--- src/controller/task/index.js
+++ src/controller/task/index.js
@@ -4,10 +4,12 @@ const validateTaskId = require("@src/middleware/validate-task-id");
 const getTaskById = require("./get-by-id");
 const listTasks = require("./list");
 const createTask = require("./create");
+const updateTask = require("./update");
 
 router.get("/", listTasks.validator, listTasks.handler);
 router.post("/", createTask.validator, createTask.handler);
 router.all("/:id", validateTaskId);
 router.get("/:id", getTaskById.handler);
+router.put("/:id", updateTask.validator, updateTask.handler);
 
 module.exports = router;
